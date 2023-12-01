fd8d07f8e38aa0aff653d2c6b68e54597bbccbde
yiannis <107484245+DimYiannis@users.noreply.github.com>
2023-12-01 19:07:13 +0200

    ibm and meta projects

---
+++ 05-meta/forIn-ForOf.js
@@ -0,0 +1,43 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+
+function logDairy() {
+    for (const product of dairy) {
+        console.log(product);
+    }
+}
+
+logDairy();
+
+// Task 2
+const animal = {
+    canJump: true
+};
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (const key of Object.keys(bird)) {
+        console.log(`${key}: ${bird[key]}`)
+    }
+};
+
+birdCan();
+
+// Task 3
+const animal2 = {
+    canJump: true
+};
+const bird = Object.create(animal2);
+bird.canFly = true;
+bird.hasFeathers = true;
+
+
+function animalCan() {
+    for (const key in bird) {
+        console.log(`${key}: ${bird[key]}`);
+    }
+}
+
+animalCan();
---
+++ 05-meta/timesTwo.test.js
@@ -0,0 +1,7 @@
+const timesTwo = require('./timesTwo');
+
+// Write the first test
+test('returns the value multiplied by num', () => {
+    expect(timesTwo(10)).toBe(20)
+});
+
